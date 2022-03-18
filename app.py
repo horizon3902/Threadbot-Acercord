025e0963068a94020c0a22b050cb048ab8c060a7
Kshitij A <kshitij.everest@gmail.com>
2022-03-19 00:12:54 +0530

    <>add_reply, <>remove_reply, <>list_replies commands added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from discord import Member
-from discord.ext.commands import has_permissions
+from discord.ext.commands import has_permissions, has_any_role
 
 
 load_dotenv()
@@ -17,6 +17,7 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="<>", help_command=None, intents=intents)
 tracking_channels = {}
 exclude_users = set()
+track_replies = {}
 
 @bot.event
 async def on_ready():
@@ -80,7 +81,65 @@ async def solve(ctx):
 @bot.command(name="help", pass_context=True)
 @has_permissions(manage_messages=True)
 async def help(ctx):
-    await ctx.message.channel.send(f"1. `<>exclude user_id`: Won't Create threads for this user.\n2. `<>include user_id`: Track an excluded user again\n3. `<>track channel_id`: This channel is tracked to create threads\n4. `<>stop channel_id`: This channel is stopped tracking\n5. `<>get_id`: Used to get id of current channel\n6. `<>close`: Close current thread (does not archive)\n7. `<>solve`: Solve current thread (archives the thread)")
+    await ctx.message.channel.send(f"1. `<>exclude user_id`: Won't Create threads for this user.\n2. `<>include user_id`: Track an excluded user again\n3. `<>track channel_id`: This channel is tracked to create threads\n4. `<>stop channel_id`: This channel is stopped tracking\n5. `<>get_id`: Used to get id of current channel\n6. `<>close`: Close current thread (does not archive)\n7. `<>solve`: Solve current thread (archives the thread)\n8. `<>add_reply prompt, reply`: Sends the `reply` when `prompt` is present in a message\n9. `<>remove_reply prompt`: Removes the reply for `prompt`\n10. `<>list_replies`: Lists all the replies")
+
+@bot.command(name="add_reply", pass_context=True)
+@has_any_role("Terminator","Skynet")
+async def add_reply(ctx, *, message):
+    msg = message.split(",")
+    if bot.command_prefix in message:
+        await ctx.message.channel.send(f"Can't have {bot.command_prefix} in message, dum dum!")
+    else:
+        track_replies.setdefault(str(ctx.message.guild).lower(),{})[(msg[0]).lower()] = msg[1]
+        await ctx.message.channel.send(f"'{msg[1]}' will be replied when '{msg[0]}' is mentioned!")
+
+@add_reply.error
+async def add_reply(ctx, error):
+    if isinstance(error, commands.MissingAnyRole):
+        text = "Sorry {}, you do not have the required role to do that!".format(ctx.message.author)
+        await ctx.message.channel.send(text)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        text = f"Missing a required argument: {error.param}"
+        await ctx.message.channel.send(text)
+
+@bot.command(name="remove_reply", pass_context=True)
+@has_any_role("Terminator","Skynet")
+async def remove_reply(ctx, *, message):
+    if bot.command_prefix in message:
+        await ctx.message.channel.send(f"Can't have {bot.command_prefix} in message, dum dum!")
+    else:
+        if (message).lower() in track_replies[str(ctx.message.guild).lower()]:
+            del track_replies[str(ctx.message.guild).lower()][(message).lower()]
+            await ctx.message.channel.send(f"'{message}' no longer tracked!")
+        else:
+            await ctx.message.channel.send("That keyword was never tracked in the first place, dum dum!")
+
+@remove_reply.error
+async def remove_reply(ctx, error):
+    if isinstance(error, commands.MissingAnyRole):
+        text = "Sorry {}, you do not have the required role to do that!".format(ctx.message.author)
+        await ctx.message.channel.send(text)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        text = f"Missing a required argument: {error.param}"
+        await ctx.message.channel.send(text)
+
+@bot.command(name="list_replies", pass_context=True)
+@has_any_role("Terminator","Skynet")
+async def list_replies(ctx):
+    if len(track_replies[str(ctx.message.guild).lower()]) == 0:
+        await ctx.message.channel.send("There are no keywords currently being tracked!")
+    else:
+        await ctx.message.channel.send("Here are the keywords currently being tracked:")
+        await ctx.message.channel.send(track_replies[str(ctx.message.guild).lower()])
+
+@list_replies.error
+async def list_replies(ctx, error):
+    if isinstance(error, commands.MissingAnyRole):
+        text = "Sorry {}, you do not have the required role to do that!".format(ctx.message.author)
+        await ctx.message.channel.send(text)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        text = f"Missing a required argument: {error.param}"
+        await ctx.message.channel.send(text)
 
 @track.error
 async def add_error(ctx, error):
@@ -147,6 +206,10 @@ async def on_message(message):
             )
         else:
             await bot.process_commands(message)
+    if str(message.guild).lower() in track_replies and message.author != bot.user and bot.command_prefix not in message.content:
+        triggers = [x for x in message.content.lower().split() if x in track_replies[str(message.guild).lower()]]
+        for trigger in triggers:
+            await message.channel.send(track_replies[str(message.guild).lower()][trigger])
     else:
         await bot.process_commands(message)
 
